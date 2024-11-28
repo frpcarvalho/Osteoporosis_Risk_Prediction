d1f423d67c82dac69a793118f92350c69576c1e9
Filipe Ricardo Pires de Carvalho <frcarvalho@ualg.pt>
2024-11-28 14:56:15 +0000

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -1,1 +1,10 @@
+import joblib
+import pandas as pd
+import numpy as np
 
+def load_model(model_path="models/stacking_ensemble_model.joblib"):
+    return joblib.load(model_path)
+
+def predict_osteoporosis_risk(data):
+    model = load_model()
+    return model.predict(data), model.predict_proba(data)
