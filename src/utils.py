5e9928eeccbe9c44a9322f1fbbbc307c447f0f3e
Filipe Ricardo Pires de Carvalho <frcarvalho@ualg.pt>
2024-11-28 14:58:09 +0000

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,9 @@
+import pandas as pd
+import numpy as np
+from sklearn.preprocessing import LabelEncoder
 
+def prepare_data(data, target_cols):
+    X = data.drop(columns=target_cols, errors='ignore')
+    for col in X.select_dtypes(include=['object']).columns:
+        X[col] = pd.to_numeric(X[col], errors='coerce')
+    return X
